801c89d5d22cce457b5de9384bd7f3bbad4f66c2
iscfranciscogallardo <isc.francisco.gallardo@gmail.com>
2023-12-05 21:16:32 -0600

    Codigo del prioyecto

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //Get: /HellowWorld
+        //public string Index()
+        //{
+        //    //return "This is my default action..."; este solo nos sirve para mostrar texto
+        //}
+
+        public IActionResult Index() { return View(); }
+
+        //Get: /HellowWorld/Welcome
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = $"Hello {name}";
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Controllers;
+using MVCApp.Data;
+using System;
+using System.Linq;
+
+namespace MVCApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCAppContext(serviceProvider.GetRequiredService<DbContextOptions<MVCAppContext>>()))
+            {
+                //Look for any movies
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.Movie.AddRange(
+                        new Movie() { Title = "When Harry Met Sally", RealeaseDate = DateTime.Parse("1989-02-12"), Genre = "Romantic Comedy", Price = 7.99M, Rating = "R" }
+                        , new Movie() { Title = "Ghostbusters", RealeaseDate = DateTime.Parse("1984-03-13"), Genre = "Comedy", Price = 8.99M, Rating = "PG-13" }
+                        , new Movie() { Title = "Ghostbusters 2", RealeaseDate = DateTime.Parse("1984-03-13"), Genre = "Comedy", Price = 9.99M, Rating = "PG-13" }
+                        , new Movie() { Title = "Rio Bravo", RealeaseDate = DateTime.Parse("1959-04-15"), Genre = "Western", Price = 3.99M, Rating = "R" }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
